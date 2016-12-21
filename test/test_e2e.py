9f63771f4e54196f9df733837fb617a02322c796
iceboy <iceboy@iceboy.org>
2016-12-20 23:49:46 -0800

    Update test_e2e.py

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -4,7 +4,7 @@ import threading
 import unittest
 import urllib.request
 
-SIMPLE_URL = b'/simple'
+SIMPLE_PATH = b'/simple'
 SIMPLE_BODY = b'<h1>It works!</h1>'
 
 
@@ -12,13 +12,13 @@ class E2eTest(unittest.TestCase):
     def setUp(self):
         config = h2o.Config()
         host = config.add_host(b'default', 65535)
-        host.add_path(SIMPLE_URL).add_handler(lambda: SIMPLE_BODY)
+        host.add_path(SIMPLE_PATH).add_handler(lambda: SIMPLE_BODY)
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
         self.sock.bind(('127.0.0.1', 0))
         self.sock.listen()
-        self.http_prefix = b'http://127.0.0.1:%d' % self.sock.getsockname()[1]
+        self.url_prefix = b'http://127.0.0.1:%d' % self.sock.getsockname()[1]
 
         self.loop = h2o.Loop()
         self.loop.start_accept(self.sock.fileno(), config)
@@ -28,11 +28,11 @@ class E2eTest(unittest.TestCase):
         while self.loop.run() == 0:
             pass
 
-    def get(self, url):
-        return urllib.request.urlopen((self.http_prefix + url).decode()).read()
+    def get(self, path):
+        return urllib.request.urlopen((self.url_prefix + path).decode()).read()
 
     def test_simple(self):
-        self.assertEqual(self.get(SIMPLE_URL), SIMPLE_BODY)
+        self.assertEqual(self.get(SIMPLE_PATH), SIMPLE_BODY)
 
 if __name__ == '__main__':
     unittest.main()
