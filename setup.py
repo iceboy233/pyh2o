28d357cf467e1baa545035a59c82532fd7fd2d9b
iceboy <me@iceboy.org>
2017-01-13 13:58:41 -0800

    Fix build on multiarch systems.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class build_ext(_build_ext):
         return os.path.abspath(path)
 
     def run(self):
-        os.environ['CFLAGS'] = os.environ.get('CFLAGS', '') + ' -fPIC'
+        os.environ['CFLAGS'] = os.environ.get('CFLAGS', '') + ' -fPIC -DWSLAY_VERSION=1'
         if (sys.platform == 'darwin' and
             'MACOSX_DEPLOYMENT_TARGET' not in os.environ and
             LooseVersion(get_config_var('MACOSX_DEPLOYMENT_TARGET') or '0') <
@@ -34,25 +34,24 @@ class build_ext(_build_ext):
 
         wslay_build_dir = self.get_temp_dir('wslay_build')
         h2o_build_dir = self.get_temp_dir('h2o_build')
-        install_dir = self.get_temp_dir('install')
 
         # Build libwslay
-        spawn(['cmake', os.path.abspath('deps/wslay'),
-               '-DCMAKE_INSTALL_PREFIX=' + install_dir], wslay_build_dir)
-        spawn(['make', 'install'], wslay_build_dir)
+        spawn(['cmake', os.path.abspath('deps/wslay')], wslay_build_dir)
+        spawn(['make', 'wslay'], wslay_build_dir)
 
         # Build libh2o-evloop
         spawn(['cmake', os.path.abspath('deps/h2o'),
-               '-DCMAKE_INSTALL_PREFIX=' + install_dir,
+               '-DWSLAY_LIBRARIES=' + os.path.abspath(os.path.join(wslay_build_dir, 'lib')),
+               '-DWSLAY_INCLUDE_DIR=' + os.path.abspath('deps/wslay/lib/includes'),
                '-DWITH_BUNDLED_SSL=ON'], h2o_build_dir)
         spawn(['make', 'libh2o-evloop'], h2o_build_dir)
 
         self.include_dirs.append('deps/h2o/include')
+        self.include_dirs.append('deps/wslay/lib/includes')
         self.include_dirs.append(os.path.join(h2o_build_dir, 'libressl-build', 'include'))
-        self.include_dirs.append(os.path.join(install_dir, 'include'))
         self.library_dirs.append(h2o_build_dir)
+        self.library_dirs.append(os.path.join(wslay_build_dir, 'lib'))
         self.library_dirs.append(os.path.join(h2o_build_dir, 'libressl-build', 'lib'))
-        self.library_dirs.append(os.path.join(install_dir, 'lib'))
         return _build_ext.run(self)
 
 
